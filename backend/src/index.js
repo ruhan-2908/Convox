75acf9435bcee5423377c340598111d424c85d7d
ruhan-2908 <ruhankb29@gmail.com>
2026-01-04 18:11:58 +0530

    Complete CRUD

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {connectDB} from './lib/db.js';
 import messageRoute from "./routes/message.route.js";
+import cors from "cors";
+
 
 dotenv.config();
 
@@ -13,6 +15,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use("/api/auth", authRoutes);
 app.use("/api/message",messageRoute);
 app.listen(process.env.PORT, () =>{
