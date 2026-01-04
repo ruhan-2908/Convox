6be9bf5091c23949da6e342680f0da1b4c1b1592
ruhan-2908 <ruhankb29@gmail.com>
2026-01-04 18:12:13 +0530

    Login UI

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,26 @@
+import {create} from "zustand";
+import {axiosInstance} from "../lib/axios.js";
+
+
+export const useAuthStore = create((set) => ({
+    authUser:null,
+    isSigningUp:false,
+    isLoggingUp:false,
+    isUpdatingProfile:false,
+
+    isCheckingAuth:true,
+
+    checkAuth : async () => {
+        try
+        {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser:res.data});
+        }catch(error)
+        {
+            console.log("Error in the check auth: ",error.message);
+            set({authUser:null})
+        }finally {
+            set({isCheckingAuth:false});
+        }
+    }
+}))
