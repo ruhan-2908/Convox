aa9ddcf3600d2250beae858ce23887b413cf986f
ruhan-2908 <ruhankb29@gmail.com>
2026-01-04 21:19:59 +0530

    Signup UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,38 @@
 import Navbar from "./components/Navbar.jsx";
-import {Routes,Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import {useAuthStore} from "./store/useAuthStore.js";
 import {useEffect} from "react";
+import {Loader} from "lucide-react";
+import {SignUpPage} from "./pages/SignUpPage.jsx";
+import {LoginPage} from "./pages/LoginPage.jsx";
+import {ProfilePage} from "./pages/ProfilePage.jsx";
+import {SettingsPage} from "./pages/SettingsPage.jsx";
+import {HomePage} from "./pages/HomePage.jsx";
 
 const App = () => {
-    const {authUser,checkAuth} = useAuthStore();
+    const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
     useEffect(() => {
         checkAuth();
     })
 
     console.log(authUser);
+
+    if (isCheckingAuth && !authUser) return (
+        <div className="flex items-center justify-center h-screen">
+            <Loader className="size-10 animate-spin"/>
+        </div>
+    )
+
     return (
         <div>
             <Navbar/>
             <Routes>
-                <Route path="/" element={<Homepage/>}/>
-                <Route path="/signup" element={<SignUpPage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
+                <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+                <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
                 <Route path="/settings" element={<SettingsPage/>}/>
-                <Route path="/profile" element={<ProfilePage/>}/>
+                <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
             </Routes>
         </div>
     )
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -22,5 +22,9 @@ export const useAuthStore = create((set) => ({
         }finally {
             set({isCheckingAuth:false});
         }
+    },
+
+    signup : async (data) => {
+
     }
 }))
